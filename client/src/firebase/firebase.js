5807e2bfcab88467cb1d359975b25f6eba14d377
dhairyavr <dhairyashah219@gmail.com>
2022-04-03 11:21:22 +0530

    api keys removed

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,17 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: `${process.env.REACT_APP_FIREBASE_APIKEY}`,
-  authDomain: "be-project-99156.firebaseapp.com",
-  projectId: "be-project-99156",
-  storageBucket: "be-project-99156.appspot.com",
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTHDOMAIN}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_APIKEY}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECTID}`,
   messagingSenderId: "579010004242",
-  appId: `${process.env.REACT_APP_FIREBASE_APPID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}`,
   measurementId: "G-TPH2YZ0VN0",
 };
 
